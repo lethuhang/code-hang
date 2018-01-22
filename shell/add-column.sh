719e47dd91a0bc6e3376693d11b0c0bffba7ca24
Hang Le <hangle.lth@gmail.com>
2018-01-22 22:04:42 +0800

    Shell script to append column

---
+++ shell/add-column.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PATH=~/Desktop/data/*.csv
+PREFIX='/Users/lethuhang/Desktop/data/processed/with-country-'
+
+for filepath in $PATH; do
+	filename=$(/usr/bin/basename $filepath)
+	country_code=$(/usr/bin/cut -d '_' -f6 <<< ${filepath} | /usr/bin/sed 's/.csv//')
+	/usr/bin/awk -F ',' \
+		-v country_code="$country_code" \
+		-v country="Country" \
+		'{
+			if ($0 ~ /"Date","Publisher","Calls","Uniques"/) {
+				print "\""country"\","$0;
+			} else {
+				print "\""country_code"\","$0;
+			}
+		}' $filepath > $PREFIX$filename
+done
